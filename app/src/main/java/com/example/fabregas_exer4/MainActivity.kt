6b7f7a9e9daf4f9ff7ec00dc120edb9bb8e68c28
Filibustero <cyfabregas@up.edu.ph>
2020-03-02 17:30:55 +0800

    feat: add SlumBook.kt that will accept the data

--- app/src/main/java/com/example/fabregas_exer4/MainActivity.kt
+++ app/src/main/java/com/example/fabregas_exer4/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.fabregas_exer4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.fabregas_exer4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.button.setOnClickListener{
+            done(it)
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/example/fabregas_exer4/SlumBook.kt
@@ -0,0 +1,4 @@
+package com.example.fabregas_exer4
+
+data class SlumBook(var name : String = "", var nickname: String = "", var age: Int = 0, var birthday : String ="", var phonenumber: String ="",
+                    var email: String = "", var course: String = "" , var dream: String = "", var message : String = "")
